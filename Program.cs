783ac8ee1beee172e0790ac0658337b9684765b3
Matheus Vinicius <118569888+maa7vini@users.noreply.github.com>
2024-08-25 22:47:02 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoPratico1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Context>
+    (options => options.UseMySql(
+        "server=localhost;initial catalog=PROJETO_PRATICO;uid=root;pwd=ma102030",
+        Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Usuario}/{action=Index}/{id?}");
+
+app.Run();
